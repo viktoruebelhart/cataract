3eecd3897ca245b761be00b08bdad16c76e8a480
Viktor Uebelhart <154932840+viktoruebelhart@users.noreply.github.com>
2024-10-07 20:58:54 +0000

    33

--- app_cataract.py
+++ app_cataract.py
@@ -5,17 +5,14 @@ import io
 from PIL import Image
 import numpy as np
 
-# Função para carregar o modelo TFLite
 @st.cache_resource
 def load_model():
-    # URL do modelo catarata em TFLite
     url = 'https://drive.google.com/uc?id=1NSsQconZZViIPqI5Z-2tWqK1AVXoN-0h'
     gdown.download(url, 'cataract_model.tflite', quiet=False)
     interpreter = tf.lite.Interpreter(model_path='cataract_model.tflite')
     interpreter.allocate_tensors()
     return interpreter
 
-# Função para carregar e processar a imagem
 def load_image():
     uploaded_file = st.file_uploader('Carregue a imagem do olho para análise', type=['png', 'jpeg', 'jpg'])
     
@@ -27,22 +24,35 @@ def load_image():
         image = image.resize((416, 416))
         image = np.array(image, dtype=np.float32) / 255.0  # Normalização
         image = np.expand_dims(image, axis=0)  # Expansão para incluir a dimensão de batch
-
+        
         return image
     else:
         return None
 
-# Função para realizar a previsão
 def forecast(interpreter, image):
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
+    # Verificação das dimensões da imagem
+    st.write(f"Forma da imagem enviada: {image.shape}")
+    st.write(f"Tipo da imagem enviada: {image.dtype}")
+
+    # Definir a forma esperada para o modelo
+    expected_shape = input_details[0]['shape']
+    st.write(f"Forma esperada de entrada pelo modelo: {expected_shape}")
+    
+    # Verifica se a forma da imagem está correta
+    if image.shape != expected_shape:
+        st.error("A forma da imagem não é compatível com a entrada do modelo.")
+        return
+    
+    # Passar a imagem processada para o modelo
     interpreter.set_tensor(input_details[0]['index'], image)
     interpreter.invoke()
     
     output_data = interpreter.get_tensor(output_details[0]['index'])
-    probability = output_data[0][0]  # Como a saída é escalar, interpretamos
-    
+    probability = output_data[0][0]
+
     # Classificação com base na probabilidade
     if probability < 0.5:
         st.write("### Classificação: Catarata Imatura")
@@ -51,7 +61,6 @@ def forecast(interpreter, image):
     
     st.write(f"### Probabilidade: {probability:.2%}")
 
-# Função principal da aplicação
 def main():
     st.set_page_config(page_title="Diagnóstico de Catarata")
     st.write("# Diagnóstico de Catarata")
